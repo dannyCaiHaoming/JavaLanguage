1e1ecf347fabb7252f608f2038a1c0078f0eee51
danny <dannycaihaoming@gmail.com>
2021-01-05 21:15:25 +0800

    1.提交 入门内容

---
+++ 1.Java快速入门/Demo.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+import jdk.nashorn.api.tree.ForInLoopTree;
+
+// https://www.liaoxuefeng.com/wiki/1252599548343744/1259544070059520
+
+public class Demo {
+    public static void main(String[] args) {
+        // name();
+        // Demo.name();
+        for (String string : args) {
+            System.out.println(string);
+        }
+    }
+
+    public static void name() {
+        var a = 5;
+        var b = 5.0;
+        /*
+        可直接使用var来定义基本变量，不需要完全像c一样，先声明变量类型。
+        */
+        var charA = 'A';
+        var charB = 'B';
+
+        char[] chars = {charA,charB};
+        var string = new String(chars);
+
+        System.out.println(string);
+
+        char[] charsArray = new char[] {'1','2'};
+        int[] intArray = {1,2,3,4};
+
+        // System.out.println(charsArray);
+        // System.out.println(intArray);
+        // for (int i : intArray) {
+        //     System.out.println(i);
+        // }
+        System.out.println(Arrays.toString(intArray));
+
+        for (int i = intArray.length-1 ; i >= 0 ; i--) {
+            System.out.println(intArray[i]);
+        }
+
+        int[][] doubleIntArray = {
+            {1,3,5},
+            {2,4,6}
+        };
+        System.out.println(Arrays.deepToString(doubleIntArray));
+    }
+
+}
