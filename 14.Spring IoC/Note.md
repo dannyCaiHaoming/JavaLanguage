3d5471989e4c5ed0fae954fffc2672aa98ec5fbc
danny <dannycaihaoming@gmail.com>
2021-01-15 19:56:11 +0800

    1. 更新

---
+++ 14.Spring IoC/Note.md
@@ -0,0 +1,6 @@
+# Spring IoC,IoC容器
+
+## Spring Io容器和Bean简介
+
+
+
--- 3.核心类/Lesson3/Demo.java
+++ 3.核心类/Lesson3/Demo.java
@@ -10,17 +10,32 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Demo3 \n");
 
+        // byte[] b1 = "Hello".getBytes();
+        // try {
+        //     byte[] b2 = "Hello".getBytes("UTF-8");
+        //     System.out.println(b2 + "\n");
+        // } catch (UnsupportedEncodingException e) {
+        //     System.out.println(e + "\n");
+        // }
+        // System.out.println(b1);
 
-        byte[] b1 = "Hello".getBytes();
-        try {
-            byte[] b2 = "Hello".getBytes("UTF-8");
-            System.out.println(b2 + "\n");
-        } catch (UnsupportedEncodingException e) {
-            System.out.println(e + "\n");
-        }
-        System.out.println(b1);
-        
     }
+
+    Demo() {
+        System.out.println("Demo init");
+    }
+
+    {
+        System.out.println("objcect print --");
+    }
+
+    int price = 110;
+
+    static {
+        System.out.println("class print --");
+    }
+
+    static int amount = 112;
 
 }
 
@@ -28,7 +43,10 @@ public class Demo {
  * Java中的String是一个引用类型，本身是一个class，但是内部是一个char[]数组。特点是不可修改
  */
 
-
- /*
-ASCII编码：
+/*
+ * ASCII编码：
  */
+
+
+
+ 
