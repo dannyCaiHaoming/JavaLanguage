d593ee00135f59c1433720c8f5667159b1ba648a
danny <dannycaihaoming@gmail.com>
2021-01-22 15:45:47 +0800

    1.更新

--- Demo/SpringIoCLearning/src/main/java/com/danny/SpringIoCLearning/SpringIoCLearningApplication.java
+++ Demo/SpringIoCLearning/src/main/java/com/danny/SpringIoCLearning/SpringIoCLearningApplication.java
@@ -1,11 +1,15 @@
 package com.danny.SpringIoCLearning;
 
+import com.danny.SpringIoCLearning.service.MailService;
 import com.danny.SpringIoCLearning.service.User;
 import com.danny.SpringIoCLearning.service.UserService1;
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 //@SpringBootApplication
 public class SpringIoCLearningApplication {
@@ -17,6 +21,10 @@ public class SpringIoCLearningApplication {
         UserService1 userService1 = context.getBean(UserService1.class);
         User user = userService1.login("bob@example.com", "password");
         System.out.println(user.getName());
+
+        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("application.xml"));
+        MailService mailService = factory.getBean(MailService.class);
+        System.out.println(mailService.getTime());
     }
 
 }
